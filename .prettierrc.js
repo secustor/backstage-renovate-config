26db4c495a3ee1a331af50ca1d455760bc4c8ada
secustor <sebastian@poxhofer.at>
2025-02-25 00:48:03 +0100

    chore: add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * TODO migrate to TS when node lts supports it without flags
+ * @type {import("prettier").Config}
+ */
+const config = {
+  plugins: ["prettier-plugin-sort-json"],
+};
+
+export default config;
